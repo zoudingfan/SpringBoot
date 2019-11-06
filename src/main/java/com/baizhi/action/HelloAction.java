824e83b6b49b81516a1c72167548c202536b9a7d
zoudingfan <57438290+zoudingfan@users.noreply.github.com>
2019-11-06 20:22:59 +0800

    Update HelloAction.java

--- src/main/java/com/baizhi/action/HelloAction.java
+++ src/main/java/com/baizhi/action/HelloAction.java
@@ -2,7 +2,7 @@ package com.baizhi.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//小邹修改一次
 @Controller
 @RequestMapping("/first")
 public class HelloAction {
